a7634ff8a9e91e77a4ebd14a8c94f388684f4c00
andreandradearke <aandrade@arkesystems.com>
2026-01-22 17:23:01 -0300

    Remove contact from i18n namespaces

--- astro-i18next.config.ts
+++ astro-i18next.config.ts
@@ -3,7 +3,7 @@ import type { AstroI18nextConfig } from "astro-i18next";
 const config: AstroI18nextConfig = {
   defaultLocale: "pt",
   locales: ["pt", "en"],
-  namespaces: ["common", "blog", "about", "contact"],
+  namespaces: ["common", "blog", "about"],
   defaultNamespace: "common",
   routes: {
     en: {
